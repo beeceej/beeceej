f20d290f929c0f68d80d84466e3eecebfc313923
Brian Jones <beeceej.code@gmail.com>
2019-05-02 22:41:54 -0500

    cl-who'ify nav component

--- nav.lisp
+++ nav.lisp
@@ -15,17 +15,21 @@
 (defmethod render ((obj nav))
   "render writes the html to a string-stream"
   (with-output-to-string (out-string)
-    (xhtmlambda:with-html-syntax-output
+    (cl-who:with-html-output
         (out-string)
-        (flet ((make-link (label ref) (xhtmlambda:a (:class "clickable" :href ref) (string label))))
-          (xhtmlambda:nav
-           (:class "navigation")
-           (xhtmlambda:div
-            (:class "flexContainer") (loop :for (label . ref) in (nav-headers obj)
-                     collect (make-link label ref )))
-           (xhtmlambda:div
-            (:class "flexContainer") (loop :for (label . ref) in (nav-icons obj)
-                     collect (make-link label ref ))))))))
+      (flet ((make-link
+                 (label ref)
+               (cl-who:htm
+                (:a :class "clickable"
+                 :href ref
+                 (cl-who:str label)))))
+        (cl-who:htm (:nav :class "navigation"
+         (:div :class "flexContainer"
+          (loop :for (label . ref) in (nav-headers obj)
+                collect (make-link label ref )))
+         (:div :class "flexContainer"
+          (loop :for (label . ref) in (nav-icons obj)
+                collect (make-link label ref )))))))))
 
 (defmethod print-object ((obj nav) out)
   (let ((fmt-string ":headers ~a :icons ~a"))
